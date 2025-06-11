34ef9fb195a00ce012091acd9a71198035c3e8d9
sukanya9 <sukanya.pasupula9@gmail.com>
2025-06-11 23:34:29 +0530

    "Updated Literals"

---
+++ src/ex_03_Literals/Lab035_Non_Primitive_Literal.java
@@ -0,0 +1,20 @@
+package ex_03_Literals;
+
+public class Lab035_Non_Primitive_Literal {
+    public static void main(String[] args) {
+        // Primitive Data Type - Defined by Java Guys
+        // // Max, Min, Size defined
+        byte b = 10; // 1 Byte -> 8 Bits
+        int age = 65; // 4 Byte -> 32 Bits
+
+        // NonPrimitive ( Defined by users) , Reference Data Types
+        // No Size, max, min
+        // Byte - 8 , Bits - 64
+        String name = "Sukanya"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+        // String, Arrays, Classes, enum...
+
+        String s = null;
+        // int age = null;
+    }
+}
---
+++ src/ex_03_Literals/Task1.java
@@ -0,0 +1,14 @@
+package ex_03_Literals;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("%d x %d = %d",num,1,num).println();
+        System.out.printf("%d x %d = %d",num,2,num*2).println();
+        System.out.printf("%d x %d = %d",num,3,num*3).println();
+        System.out.printf("%d x %d = %d",num,4,num*4).println();
+        System.out.println("...");
+        System.out.printf("%d x %d = %d",num,10,num*10).println();
+    }
+}
+
