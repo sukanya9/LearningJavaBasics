1f36fbc3def067a0bd77efb834afc96ed318cdda
sukanya9 <sukanya.pasupula9@gmail.com>
2025-06-23 16:57:18 +0530

    "JavaPrograms_Assessment_PrimitiveDataType"

---
+++ src/Assesment1_Programs/Q1_PrimitiveDataType.java
@@ -0,0 +1,34 @@
+package Assesment1_Programs;
+
+public class Q1_PrimitiveDataType {
+    public static void main(String[] args) {
+        // Integer types
+        byte byteVar = 127;
+        short shortVar = 32;
+        int integer = 25;
+        long longVar = 100L;
+
+        // Floating-point types
+        float pi = 3.14f;
+        double doubleVar = 3.14159265359;
+        // Character type
+        char charVar = 'A';          // Single character
+
+        // Boolean type
+        boolean b = true;      // Boolean value
+
+        // String (not a primitive type, but commonly used)
+        String str = "Hello, Java!";
+
+        // Displaying the variables
+        System.out.println("Byte Value: " + byteVar);
+        System.out.println("Short Value: " + shortVar);
+        System.out.println("Integer Value: " + integer);
+        System.out.println("Long Value: " + longVar);
+        System.out.println("Float Value: " + pi);
+        System.out.println("Double Value: " + doubleVar);
+        System.out.println("Character Value: " + charVar);
+        System.out.println("Boolean Value: " + b);
+        System.out.println("String Value: " + str);
+    }
+}
